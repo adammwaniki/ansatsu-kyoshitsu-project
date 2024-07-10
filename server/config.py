d02c6e9f9485a9098d1b040299b192b1bef084de
Adam Mwaniki <adamndegwa@protonmail.com>
2024-07-10 23:23:46 +0300

    Adds password hashing, login and sign up resources

--- server/config.py
+++ server/config.py
@@ -8,6 +8,7 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from flask_bcrypt import Bcrypt
 
 # Local imports
 
@@ -15,6 +16,7 @@ from sqlalchemy import MetaData
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.json.compact = False
 
 # Define metadata, instantiate db
@@ -30,3 +32,6 @@ api = Api(app)
 
 # Instantiate CORS
 CORS(app)
+
+# Instantiate Bcrypt
+bcrypt = Bcrypt(app)
